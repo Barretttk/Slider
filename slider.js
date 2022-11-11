fa726a0df4f7b5de578c2819c52e5fb07d09ff92
Timothy Barrett <Timothy.barrett@hotmail.com>
2022-11-11 04:23:14 -0500

    finished project

--- slider.js
+++ slider.js
@@ -9,6 +9,31 @@ const bottom  = document.querySelector(".bottom")
 let slideNumber = 1;
 const length = images.length;
 
+
+for (let i = 0; i < length; i++) {
+  const div = document.createElement("div");
+  div.className = "button";
+  bottom.appendChild(div);
+}
+
+const buttons = document.querySelectorAll(".button");
+buttons[0].style.backgroundColor = "black";
+
+const resetButton = () => {
+  buttons.forEach((button) => {
+    button.style.backgroundColor = "transparent";
+  });
+};
+
+buttons.forEach((button, i) => {
+  button.addEventListener("click", () => {
+    resetButton();
+    slider.style.transform = `translateX(-${i * 800}px)`;
+    slideNumber = i + 1;
+    button.style.backgroundColor = "black";
+  });
+});
+
 const nextSlide = () => {
     slider.style.transform = `translateX(-${slideNumber * 800}px)`;
     slideNumber ++;
@@ -27,7 +52,6 @@ const getFirstSlide = () => {
 const getLastSlide = () => {
     slider.style.transform = `transslatesX({(length - 1)8 800}px)`;
     slideNumber = length;
-
 }
 
 // first click is 800 = 800*1
@@ -35,39 +59,21 @@ const getLastSlide = () => {
 // third click is 2400 = 800*3
 // forth click is 3200 = 800*4
 
+const colorChange = () => {
+    resetButton();
+    buttons [slideNumber -1].style.backgroundColor = "black";
+};
 
 Right.addEventListener( "click",() => {
     
-    slideNumber < length ? nextSlide()
-    :
-    getFirstSlide()
-    
+    slideNumber < length ? nextSlide() : getFirstSlide()
+    colorChange();
 });
-
 
 Left.addEventListener( "click",() => {
 
     slideNumber > 1 ? prevSlide() : getLastSlide()
+    colorChange();
 });
 
-for (let i = 0; i < length; i++){
-    const div = document.createElement("div")
-    div.className = "button"
-    bottom.appendChild(div)
-}
 
-const buttons = document.querySelectorAll(".button")
-buttons[0].style.backgroundColor ="black";
-
-const resetButton = () => {
-    buttons.forEach(button =>{
-        button.style.backgroundColor = "transparent";
-    });
-};
-
-buttons.forEach((button, i) => {
-    button.addEventListener( "click",() => {
-        resetButton();
-        slider.style.transform = `translateX(-${i * 800}px)`;
-    });
-});
